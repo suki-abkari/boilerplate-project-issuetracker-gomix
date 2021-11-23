a71d4687832b7994dd02e6150f5badca8eb00fc6
Suki <devacount.ab220@gmail.com>
2021-11-23 13:23:24 +0100

    POST f(x)

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # [Issue Tracker](https://www.freecodecamp.org/learn/quality-assurance/quality-assurance-projects/issue-tracker)
 
+### Requirement
+npm install mongodb@latest
+npm install mongoose@latest
 
+
+### GET
+
+### POST
+The post function() should return an object with the required fields
+<img src="public/img/post_output.png" />
+
+### PUT 
+
+### DELETE
+
+
+## [ Demo](https://boilerplate-project-metricimpconverter.sukainaabkari.repl.co/)
--- routes/api.js
+++ routes/api.js
@@ -8,23 +8,26 @@ const uri = process.env['MONGO_URI']
 'use strict';
 
 module.exports = function(app) {
-  mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   let issueSchema = new mongoose.Schema({
-    issue_title: {type: String, required: true},
-    issue_text: {type: String, required: true},
-    created_by : {type: String, required: true},
-    assigned_to : String,
-    status_text : String,
-    open: {type: Boolean, required: true},
-    created_on: {type: Date, required: true},
-    updated_on: {type: Date, required: true},
+    issue_title: { type: String, required: true },
+    issue_text: { type: String, required: true },
+    created_by: { type: String, required: true },
+    assigned_to: String,
+    status_text: String,
+    open: { type: Boolean, required: true },
+    created_on: { type: Date, required: true },
+    updated_on: { type: Date, required: true },
     project: String
   })
-  
+
   let Issue = mongoose.model('Issue', issueSchema)
+  console.log(Issue)
 
   app.route('/api/issues/:project')
-
     .get(function(req, res) {
       let project = req.params.project;
 
@@ -32,7 +35,23 @@ module.exports = function(app) {
 
     .post(function(req, res) {
       let project = req.params.project;
-
+      let addIssue = new Issue({
+        issue_title: req.body.issue_title,
+        issue_text: req.body.issue_text,
+        created_by: req.body.created_by,
+        assigned_to: req.body.assigned_to || '',
+        status_text: req.body.status_text || '',
+        open: true,
+        created_on: new Date().toUTCString(),
+        updated_on: new Date().toUTCString(),
+        project: project
+      })
+      addIssue.save((error, savedIssue) => {
+        if (!error && savedIssue) {
+          console.log('Saved Issue is')
+          console.log(savedIssue)
+        }
+      })
     })
 
     .put(function(req, res) {
