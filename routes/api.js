4944a1f265facdbe307552bc8b8ef473fe4204b0
Suki <devacount.ab220@gmail.com>
2021-11-23 12:31:53 +0100

    db connection & schema

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,48 @@
+
+const expect = require('chai').expect;
+const MongoClient = require('mongodb');
+const ObjectId = require('mongodb').ObjectID;
+const mongoose = require('mongoose');
+const uri = process.env['MONGO_URI']
+
 'use strict';
 
-module.exports = function (app) {
+module.exports = function(app) {
+  mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  let issueSchema = new mongoose.Schema({
+    issue_title: {type: String, required: true},
+    issue_text: {type: String, required: true},
+    created_by : {type: String, required: true},
+    assigned_to : String,
+    status_text : String,
+    open: {type: Boolean, required: true},
+    created_on: {type: Date, required: true},
+    updated_on: {type: Date, required: true},
+    project: String
+  })
+  
+  let Issue = mongoose.model('Issue', issueSchema)
 
   app.route('/api/issues/:project')
-  
-    .get(function (req, res){
+
+    .get(function(req, res) {
       let project = req.params.project;
-      
+
     })
-    
-    .post(function (req, res){
+
+    .post(function(req, res) {
       let project = req.params.project;
-      
+
     })
-    
-    .put(function (req, res){
+
+    .put(function(req, res) {
       let project = req.params.project;
-      
+
     })
-    
-    .delete(function (req, res){
+
+    .delete(function(req, res) {
       let project = req.params.project;
-      
+
     });
-    
+
 };
